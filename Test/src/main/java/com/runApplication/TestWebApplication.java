bdefc49169b79c51a98bfb2c3fe930850989e8f7
chenxinghao1994 <542013089@qq.com>
2020-12-14 14:05:53 +0800

    111

--- Test/src/main/java/com/runApplication/TestWebApplication.java
+++ Test/src/main/java/com/runApplication/TestWebApplication.java
@@ -3,7 +3,7 @@ package com.runApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//SpringBoot 启动时会自动注入数据源和配置 jpa
+@SpringBootApplication//SpringBoot 启动时会自动注入数据源和配置 jpa 陈行昊
 public class TestWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestWebApplication.class, args);
